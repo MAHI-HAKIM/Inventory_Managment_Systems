ddd5b6fe920e1a063e935bcfed72a43f771a46fa
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-07-08 17:24:24 +0300

    Created the first Login Form just for a tryout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,38 +24,130 @@ namespace Inventory_Managment_System
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_txt = new System.Windows.Forms.TextBox();
+            this.password_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.exit_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // label1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(475, 210);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // username_txt
+            // 
+            this.username_txt.Location = new System.Drawing.Point(246, 84);
+            this.username_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.username_txt.Name = "username_txt";
+            this.username_txt.Size = new System.Drawing.Size(259, 22);
+            this.username_txt.TabIndex = 2;
+            // 
+            // password_txt
+            // 
+            this.password_txt.Location = new System.Drawing.Point(246, 129);
+            this.password_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.password_txt.Name = "password_txt";
+            this.password_txt.Size = new System.Drawing.Size(259, 22);
+            this.password_txt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.label3.Location = new System.Drawing.Point(260, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 57);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "WELCOME";
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Location = new System.Drawing.Point(246, 176);
+            this.clear_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(94, 40);
+            this.clear_btn.TabIndex = 5;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(411, 176);
+            this.login_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(94, 40);
+            this.login_btn.TabIndex = 6;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(326, 244);
+            this.exit_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(94, 43);
+            this.exit_btn.TabIndex = 7;
+            this.exit_btn.Text = "Exit";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(685, 358);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password_txt);
+            this.Controls.Add(this.username_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username_txt;
+        private System.Windows.Forms.TextBox password_txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button clear_btn;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Button exit_btn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Inventory_Managment_System
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=HP-ENVY-MAHI\MSSQLSERVER03;Initial Catalog=Practice;Integrated Security=True");
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            string Username = "", Password = "";
+
+            Username = username_txt.Text;
+            Password = password_txt.Text;
+
+            try
+            {
+                String querry = "Select * From Login Where Username = '" + username_txt.Text + "' AND Password = '" + password_txt.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    Username = username_txt.Text;
+                    Password = password_txt.Text;
+
+                    ////////////////
+                    Welcome welc = new Welcome();
+
+                    welc.Show();
+                    this.Hide();
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //username_txt.Clear();
+                    password_txt.Clear();
+
+                    username_txt.Focus();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+
+            username_txt.Clear();
+            password_txt.Clear();
+
+            username_txt.Focus();
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            res = MessageBox.Show("Do you want to exit", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                this.Show();
+                username_txt.Focus();
+            }
         }
     }
 }
