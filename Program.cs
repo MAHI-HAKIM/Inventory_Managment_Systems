560dbecb4730381d6bdd434d6c5e341ff2bee769
ammar <ammarmtofik@yahoo.com>
2023-07-11 23:08:27 +0300

    changed main

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ using System.Windows.Forms;
 namespace Inventory_Managment_System
 {
     static class Program
-    {
+    { 
+        // helllo
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +17,7 @@ namespace Inventory_Managment_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SignUp_Form());
         }
     }
 }
